678a1b208fda7a03c895ddc07cc0b6271ebc109b
Akmal <ahmad6akmal99@gmail.com>
2020-12-23 06:47:39 +0000

    Updating the codes and adding some maths operations

--- 6-3.py
+++ 6-3.py
@@ -1,32 +1,20 @@
 import socket
-import time
-import math
-import os
 import sys
+import time
+import errno
+import math
 import colorama
-from colorama import Fore
+from colorama import Fore, Style
+from multiprocessing import Process
 
-serverSocket = socket.socket()
 
-host = ''
-port = 8889
-ThreadCount = 0
-
-serverSocket.bind((host,port))
-
-print('\nWaiting for connection...')
-serverSocket.listen(3)
-colorama.init()
-print(Fore.CYAN)
-def calc(clientSocket, addr):
+def process_start(clientSocket,addr):
 	clientSocket.send(str.encode('Welcome to the Server\n'))
 	while True:
-
 		print('\nConnected to ', addr)
 
 		data = clientSocket.recv(1024)
 		num = clientSocket.recv(1024)
-
 
 		if data == b'1':
 			print(data.decode('utf-8') + '. Logarithm Process')
@@ -37,21 +25,43 @@ def calc(clientSocket, addr):
 		elif data == b'3':
 			print(data.decode('utf-8') + '. Exponential Process')
 			answer = math.frexp(float(num))
+		elif data == b'4':
+			num2 = clientSocket.recv(1024)
+			print(data.decode('utf-8') + '. Multiplication Process')
+			answer = (float(num) * float(num2))
+		elif data == b'5':
+			num2 = clientSocket.recv(1024)
+			print(data.decode('utf-8') + '. Division Process')
+			answer = (float(num) / float(num2))
 		else:
-			print('Connection Ended....')
+			print('Connection Ended For ' + str(addr) + '\n')
 			False
 			break
 
 		convert = str(answer)
 		clientSocket.send(convert.encode('utf-8'))
+	clientSocket.close()
 
-while True:
-	Client, address = serverSocket.accept()
-	if(calc(Client,address)):
-		pass
-	else:
-		False
-		break
-
-serverSocket.close()
-
+if __name__ == '__main__':
+	host = ''
+	port = 8888
+	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	colorama.init()
+	print(Fore.CYAN + Style.BRIGHT)
+	serverSocket.bind((host,port))
+	print("listening...")
+	serverSocket.listen(3)
+	try:
+		while True:
+			try:
+				client, addr = serverSocket.accept()
+				p = Process(target=process_start, args=(client,addr))
+				p.start()
+			except socket.error:
+				print('got a socket error')
+	except Exception as e:
+		print('an exception occurred!')
+		print(e)
+		sys.exit(1)
+	finally:
+		serverSocket.close()
